8500f3c9b84a0c05710c03049cce4986b2513f02
nguyenducmy <nguyenducmy393@gmail.com>
2023-03-16 22:22:58 +0700

    upate footer

--- footer.php
+++ footer.php
@@ -1,9 +1,11 @@
 <div id="pageTop">
-    <img src="<?php echo get_template_directory_uri() ?>/images/common/pagetop.png" class="page_top_img" />
+  <img src="<?php echo get_template_directory_uri() ?>/images/common/pagetop.png" class="page_top_img" />
 </div>
 <footer>
-    footer
+  Copyright 2023 - All right is reserved
 </footer>
 <!--objedt-fit（IE対策）-->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/object-fit-images/3.2.3/ofi.js"></script>
-<script>objectFitImages();</script>
+<script>
+objectFitImages();
+</script>
